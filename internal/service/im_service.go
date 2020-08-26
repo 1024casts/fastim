50af92f875636ad025df3e90cf23454abc25028e
easygogo <quanlongwang@gmail.com>
2020-08-26 20:13:22 +0800

    feat: add msg const

--- internal/service/im_service.go
+++ internal/service/im_service.go
@@ -5,6 +5,28 @@ import (
 	"github.com/1024casts/fastim/internal/dao"
 )
 
+const (
+	// 最大消息id
+	MaxMsgID = 0xffffffffffff
+
+	// 基础消息类型 1~20
+	MsgTypeText  int = 1 //文本消息
+	MsgTypePic   int = 2 //图片消息
+	MsgTypeVoice int = 3 //语音消息
+	MsgTypeVideo int = 4 //视频消息
+
+	// 系统消息类型 51~100
+	MsgTypeNoticeText int = 51 // 文字通知
+
+	// 业务消息类型 101+
+	MsgTypeInviteJoinClass int = 101 // 文章卡片消息
+
+	// 接收类型
+	ReceiveTypeBoth = 0 // 双方接收
+	ReceiveTypePeer = 1 // 仅对方接收
+	ReceiveTypeSelf = 2 // 仅自己接收
+)
+
 // 直接初始化，可以避免在使用时再实例化
 //var UserService = NewUserService()
 
